100f2a3d1fb58bbfecdf93a6700672e6ec2a57f4
Jon Jungler <yujiangctrc@163.com>
2018-05-19 12:54:02 +0800

    520
    520

---
+++ 520/js/functions.js
@@ -0,0 +1,160 @@
+﻿
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+$(function () {
+    // setup garden
+	$loveHeart = $("#loveHeart");
+    $loveHeart.height($loveHeart.width() * 0.9);
+    $loveHeartW = $loveHeart.width();
+    $loveHeartH = $loveHeart.height();
+    $garden = $("#garden");
+    gardenCanvas = $garden[0];
+	gardenCanvas.width = $("#loveHeart").width();
+    gardenCanvas.height = $("#loveHeart").height()
+    gardenCtx = gardenCanvas.getContext("2d");
+    gardenCtx.globalCompositeOperation = "lighter";
+    garden = new Garden(gardenCtx, gardenCanvas);
+
+    // renderLoop
+    setInterval(function () {
+        garden.render();
+    }, Garden.options.growSpeed);
+
+    var together = new Date();
+    together.setFullYear(2018, 2, 17);
+    together.setHours(20);
+    together.setMinutes(35);
+    together.setSeconds(0);
+    together.setMilliseconds(0);
+
+    if (!document.createElement('canvas').getContext) {
+        var msg = document.createElement("div");
+        msg.id = "errorMsg";
+        msg.innerHTML = "Your browser doesn't support HTML5!<br/>Recommend use Chrome 14+/IE 9+/Firefox 7+/Safari 4+";
+        document.body.appendChild(msg);
+        $("#code").css("display", "none")
+        $("#copyright").css("position", "absolute");
+        $("#copyright").css("bottom", "10px");
+        document.execCommand("stop");
+    } else {
+        setTimeout(function () {
+            startHeartAnimation();
+        }, 500);
+
+        timeElapse(together);
+        setInterval(function () {
+            timeElapse(together);
+        }, 500);
+
+        // adjustCodePosition();
+        $("#code").typewriter();
+    }
+});
+
+$(window).resize(function() {
+    var newWidth = $(window).width();
+    var newHeight = $(window).height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+function getHeartPoint(angle) {
+	var t = angle / Math.PI;
+	var x = 19 * ($loveHeartW / 675) * (16 * Math.pow(Math.sin(t), 3));
+	var y = - 20 * ($loveHeartH / 620) * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+	return new Array(_offsetX + x, _offsetY + y);
+}
+
+function startHeartAnimation() {
+	var interval = 50;
+	var angle = 10;
+	var heart = new Array();
+	var animationTimer = setInterval(function () {
+		var bloom = getHeartPoint(angle);
+		var draw = true;
+		for (var i = 0; i < heart.length; i++) {
+			var p = heart[i];
+			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+			if (distance < Garden.options.bloomRadius.max * 1.3) {
+				draw = false;
+				break;
+			}
+		}
+		if (draw) {
+			heart.push(bloom);
+			garden.createRandomBloom(bloom[0], bloom[1]);
+		}
+		if (angle >= 30) {
+			clearInterval(animationTimer);
+			showMessages();
+		} else {
+			angle += 0.2;
+		}
+	}, interval);
+}
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function timeElapse(date){
+	var current = Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var result = "<span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒";
+	$("#elapseClock").html(result);
+}
+
+function showMessages() {
+	// adjustWordsPosition();
+	$('#words').fadeIn(5000);
+}
+
+// function adjustWordsPosition() {
+// 	$('#words').css("position", "absolute");
+// 	$('#words').css("top", $("#garden").position().top + 195);
+// 	$('#words').css("left", $("#garden").position().left + 70);
+// }
+
+function adjustCodePosition() {
+	$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
+}
+
+function showLoveU() {
+	$('#loveu').fadeIn(3000);
+}
--- proxyMiddleWare.js
+++ proxyMiddleWare.js
@@ -6,16 +6,8 @@ const port = '3000';
 const path = require('path');
 const proxyMiddleWare = require("http-proxy-middleware");
 
-// const proxyPath = 'http://10.45.9.137:8080'; // 测试服务器
-// const proxyPath = 'http://10.45.10.17:8080'; // demo测试服务器
-const proxyPath = 'http://10.45.10.9:8080';// UAT 服务器
-// const proxyPath = 'http://10.45.16.110:8080'; // 向东服务器
-// const proxyPath = 'http://10.45.16.151:8080'; // 葛丹服务器
-// const proxyPath = 'http://10.64.230.232:8080'; // 葛丹服务器
-
-const proxyOption ={target:proxyPath,changeOrigoin:true};
 const app = express();
-app.use(express.static(path.join(__dirname,'./app')));
+app.use(express.static(path.join(__dirname)));
 
 // app.use("/ajax",proxyMiddleWare(proxyOption));
 
